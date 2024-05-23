015457ccc20a4bef5cd6a49ffbca34d1bc4a807e
danmaraba <danielkmaraba@gmail.com>
2024-05-23 13:06:41 +0300

    added images to menclothing array

--- app.js
+++ app.js
@@ -3,84 +3,84 @@ const menClothing=[
         id:1,
         category:'Polo',
         price:'$40',
-        image:'',
+        image:'./images/istockphoto-1126630118-2048x2048.jpg',
         description:'Lorem ipsum dolor sit amet consectetur.'
     },
     {
         id:2,
         category:'T-shirt',
         price:'$30',
-        image:'',
+        image:'./images/istockphoto-1151955707-170667a.webp',
         description:'Lorem ipsum dolor sit amet consectetur.'
     },
     {
         id:3,
         category:'T-shirt',
         price:'$25',
-        image:'',
+        image:'./images/istockphoto-1490616219-170667a.webp',
         description:'Lorem ipsum dolor sit amet consectetur.'
     },
     {
         id:4,
         category:'Polo',
         price:'$50',
-        image:'',
+        image:'./images/istockphoto-1496823090-170667a.webp',
         description:'Lorem ipsum dolor sit amet consectetur.'
     },
     {
         id:5,
         category:'Shirt',
         price:'$40',
-        image:'',
+        image:'./images/photo-1598032895397-b9472444bf93.avif',
         description:'Lorem ipsum dolor sit amet consectetur.'
     },
     {
         id:6,
         category:'Shirt',
         price:'$70',
-        image:'',
+        image:'./images/photo-1598033129183-c4f50c736f10.avif',
         description:'Lorem ipsum dolor sit amet consectetur.'
     },
     {
         id:7,
         category:'Pant',
         price:'$35',
-        image:'',
+        image:'./images/istockphoto-532278616-170667a.webp',
         description:'Lorem ipsum dolor sit amet consectetur.'
     },
     {
         id:8,
         category:'Pant',
         price:'$47',
-        image:'',
+        image:'./images/istockphoto-508549005-170667a.webp',
         description:'Lorem ipsum dolor sit amet consectetur.'
     },
     {
         id:9,
         category:'Short',
         price:'$75',
-        image:'',
+        image:'./images/istockphoto-504371432-170667a.webp',
         description:'Lorem ipsum dolor sit amet consectetur.'
     },
     {
         id:10,
         category:'Jeans',
         price:'$45',
-        image:'',
+        image:'./images/istockphoto-527236518-170667a.webp',
         description:'Lorem ipsum dolor sit amet consectetur.'
     },
     {
         id:11,
         category:'Jeans',
         price:'$56',
-        image:'',
+        image:'./images/istockphoto-1185850452-170667a.webp',
         description:'Lorem ipsum dolor sit amet consectetur.'
     },
     {
         id:12,
         category:'Short',
         price:'$37',
-        image:'',
+        image:'./images/istockphoto-175438283-170667a.webp',
         description:'Lorem ipsum dolor sit amet consectetur.'
     }
 ]
