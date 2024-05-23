ed659234e0bbe7a89dec00029424a25f96f5c02b
danmaraba <danielkmaraba@gmail.com>
2024-05-23 12:54:57 +0300

    created an array of objects

---
+++ app.js
@@ -0,0 +1,86 @@
+const menClothing=[
+    {
+        id:1,
+        category:'Polo',
+        price:'$40',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+        id:2,
+        category:'T-shirt',
+        price:'$30',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+        id:3,
+        category:'T-shirt',
+        price:'$25',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+        id:4,
+        category:'Polo',
+        price:'$50',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+        id:5,
+        category:'Shirt',
+        price:'$40',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+        id:6,
+        category:'Shirt',
+        price:'$70',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+        id:7,
+        category:'Pant',
+        price:'$35',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+        id:8,
+        category:'Pant',
+        price:'$47',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+        id:9,
+        category:'Short',
+        price:'$75',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+        id:10,
+        category:'Jeans',
+        price:'$45',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+        id:11,
+        category:'Jeans',
+        price:'$56',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    },
+    {
+        id:12,
+        category:'Short',
+        price:'$37',
+        image:'',
+        description:'Lorem ipsum dolor sit amet consectetur.'
+    }
+]
